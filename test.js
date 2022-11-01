140b62089f4181c9f2e45d3d07acfc47dc1402c5
chan kang <chan@memorahealth.com>
2022-11-01 00:06:18 -0400

    clean up unused code

--- test.js
+++ test.js
@@ -3,21 +3,7 @@ const path = require('path');
 const app = express()
 const port = 5001
 
-// app.use(helmet.contentSecurityPolicy({
-//   directives: {
-//     frameAncestors: ["http://localhost:"]
-//   }
-// }))
-
 app.get('/', 
-(req, res, next) => {
-  res.set('X-Frame-Options', 'DENY')
-  // res.set('X-Frame-Options', 'ALLOW-FROM http://bore.pu')
-  // res.set('X-Frame-Options', 'http://blah:41065')
-  // Content-Security-Policy: frame-ancestors <source>;
-  res.set('Content-Security-Policy', 'frame-ancestors http://bore.pub:42323')
-  next()
-},
  (req, res) => {
   console.log(req.originalUrl)
   // res.send('Hello World!')
@@ -25,5 +11,5 @@ app.get('/',
 })
 
 app.listen(port, () => {
-  console.log(`Example app to be embedded listening on port ${port}`)
+  console.log(`Example app to embed pillpal at ${port}...`)
 })
